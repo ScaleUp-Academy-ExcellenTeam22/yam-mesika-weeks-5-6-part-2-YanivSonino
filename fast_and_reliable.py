0072f1d95fa05315d7872a05dcfa3d546ba3f8db
Janiv <y1a2n3i4v55@gmail.com>
2022-04-05 19:11:02 +0300

    task completed, check what is faster to search in set or list

---
+++ fast_and_reliable.py
@@ -0,0 +1,20 @@
+import time
+
+
+def average_runtime(structure):
+    time_to_check = 1000
+    start_time = time.time()
+    for i in range(time_to_check):
+        'zwitterion\n' in structure
+    return (time.time() - start_time) / time_to_check
+
+
+file = open("words.txt", 'r')
+words_list = []
+words_set = set()
+for word in file:
+    words_list.append(word)
+    words_set.add(word)
+
+print(average_runtime(words_list))
+print(average_runtime(words_set))
