cca8bfdd9144df0776a7c9d026b04c2d746f9b71
Janiv <y1a2n3i4v55@gmail.com>
2022-04-05 16:21:41 +0300

    Task is finish, interleave is implemented with generator

---
+++ integrated_tools.py
@@ -0,0 +1,30 @@
+def interleave(*integrated):
+    i = 0
+    maxi = len(max(integrated, key=len))
+    while i < maxi:
+        for item in integrated:
+            if i >= len(item):
+                continue
+            yield item[i]
+        i += 1
+
+# interLeave without generator
+# def interleave(*integrated):
+#     lst = []
+#     i = 0
+#     maxi = len(max(integrated, key=len))
+#     while i < maxi:
+#         for item in integrated:
+#             if i >= len(item):
+#                 continue
+#             lst.append(item[i])
+#         i += 1
+#     return lst
+#
+
+
+inter_leave_list = []
+generator_leave = interleave('abc', [1, 2, 3], ('!', '@', '#'))
+for item in generator_leave:
+    inter_leave_list.append(item)
+print(inter_leave_list)
