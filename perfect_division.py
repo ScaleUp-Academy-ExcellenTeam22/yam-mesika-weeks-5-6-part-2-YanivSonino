cbe8a37bd952155d8ba3c8c8d9a3e4ba522e6f0c
Janiv <y1a2n3i4v55@gmail.com>
2022-04-05 21:11:02 +0300

    fixed and main added

--- perfect_division.py
+++ perfect_division.py
@@ -2,7 +2,7 @@ from math import sqrt
 
 
 def div(number_to_check):
-    for i in range(1, number_to_check):
+    for i in range(1, int((number_to_check / 2)) + 1):
         if number_to_check % i == 0:
             yield i
 
@@ -12,17 +12,16 @@ def check_for_perfection(number_to_check):
     generator = div(number_to_check)
     for number in generator:
         Sum += number
-        if Sum > number_to_check:
-            return False
-        elif Sum == number_to_check:
-            return number_to_check
+    if Sum > number_to_check:
+        return False
+    elif Sum == number_to_check:
+        return number_to_check
 
 
-num = 1
-
-while 1:  # check for all numbers
-    is_perfect = check_for_perfection(num)
-    if is_perfect == num:
-        print(is_perfect)
-    num += 1
-
+if __name__ == '__main__':
+    num = 1
+    while 1:  # check for all numbers
+        is_perfect = check_for_perfection(num)
+        if is_perfect == num:
+            print(is_perfect)
+        num += 1
